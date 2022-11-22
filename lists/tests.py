99ef97e9a2ce5b8634f909fbb762010df536fd32
stalkerist7474 <80361659+stalkerist7474@users.noreply.github.com>
2022-11-23 00:33:06 +0300

    минимальный html

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from lists.views import home_page
+from django.http import HttpRequest
 #
 
 class HomePageTest(TestCase):
@@ -12,4 +13,12 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
 
-    
+    def test_home_page_returns_correct_html(self):
+        '''тест: домашняя страница возвращает правильный html'''
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
