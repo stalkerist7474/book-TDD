4c086940f2371fa3f5cb4420f8fbb05942513d9e
stalkerist7474 <80361659+stalkerist7474@users.noreply.github.com>
2022-12-04 23:47:31 +0300

    рефакторизация теста

--- lists/tests.py
+++ lists/tests.py
@@ -6,19 +6,30 @@ from django.http import HttpRequest
 
 class HomePageTest(TestCase):
     '''Тест домашней страницы'''
+    def test_uses_home_template(self):
+        '''тест: используется домашний шаблон'''
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_root_url_resolves_to_home_page_view(self):
-        '''тест: корневой url преобразуется в представление домашней страницы'''
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
 
-    def test_home_page_returns_correct_html(self):
-        '''тест: домашняя страница возвращает правильный html'''
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
 
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+    #def test_root_url_resolves_to_home_page_view(self):
+    #    '''тест: корневой url преобразуется в представление домашней страницы'''
+    #    found = resolve('/')
+    #    self.assertEqual(found.func, home_page)
+
+
+    #def test_home_page_returns_correct_html(self):
+    #    '''тест: домашняя страница возвращает правильный html'''
+        
+    #    response = self.client.get('/')
+    #    html = response.content.decode('utf8')
+
+    #    self.assertTrue(html.startswith('<html>'))
+    #    self.assertIn('<title>To-Do lists</title>', html)
+    #    self.assertTrue(html.strip().endswith('</html>'))
+
+    #    self.assertTemplateUsed(response, 'home.html')
+
+    
